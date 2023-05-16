3135fe1f584b438ad5e52afa6ee314b8ab4bf422
Alan-Gonzalez33 <alan.learning03@gmail.com>
2023-05-16 02:50:27 +0000

    Docker y CI/CD

---
+++ sample_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+sample = Flask(__name__)
+
+@sample.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    sample.run(host="10.0.2.15", port=9999)
+# Add to this file for the sample app lab
